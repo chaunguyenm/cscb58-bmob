0fa21afdba8b76c016ef1d635ad840c474075672
chaunguyenm <chaunguyenm03@gmail.com>
2023-04-02 01:47:46 -0400

    updated milestone 2: basic controls
    - player can move left and right
    - can detect collision with enemies

--- game.asm
+++ game.asm
@@ -55,67 +55,66 @@
 .eqv SHOOT 115
 .eqv RESET 112
 
-# Other
+# Size
 .eqv SIZE_BY_UNIT 64		# Size of bitmap display by buffer unit
 .eqv SIZE_BY_BYTE 256		# Size of one bitmap display row by byte
-.eqv SLEEP_TIME 1000		# Sleeping time in miliseconds
+.eqv ENEMY_HEIGHT 3
+.eqv PLAYER_HEIGHT 6
+.eqv PLAYER_LEFT 2
+.eqv PLAYER_RIGHT 2
+.eqv BOMB_HEIGHT 3
+
+.eqv SLEEP_TIME 40		# Sleeping time in miliseconds
+.eqv JUMP_HEIGHT 12		# How high player can jump
 
 
 .data
+playerState:	.word		1000:3
+enemyState:	.word		0:7
+bombState:	.word		0:7
+platformState:	.word		0:7
+
+# Debug messages
+newline:	.asciiz		"\n"
 
 .text
 .globl main
-main:		# Draw first platform
+setup:		jal erase_screen
+
+		# Draw first platform
  		li $a0, 0			# Store x coordinate
 		li $a1, 17			# Store y coordinate
 		jal bitmap_address		# Compute start address
+		la $s1, platformState		# Load memory address of platformState
+		li $a1, 3			# Number of platforms for this level
+		sw $a1, 0($s1)			# Store number of platforms at start of array platformState
+		sw $s0, 4($s1)			# Store location of first platform into memory
 		add $a0, $s0, $zero		# Store start address
-		li $a1, 21			# Store size of platform
+		li $a1, 21			# Declare size of platform
+		sw $a1, 8($s1)			# Store size of platform
 		jal draw_platform		# Draw platform
 		
 		# Draw second platform
 		li $a0, 32			# Store x coordinate
 		li $a1, 30			# Store y coordinate
 		jal bitmap_address		# Compute start address
+		la $s1, platformState		# Load memory address of platformState
+		sw $s0, 12($s1)			# Store location of second platform into memory
 		add $a0, $s0, $zero		# Store start address
-		li $a1, 32			# Store size of platform
+		li $a1, 32			# Define size of platform
+		sw $a1, 16($s1)			# Store size of platform into memory
 		jal draw_platform		# Draw platform
 
 		# Draw third platform
 		li $a0, 9			# Store x coordinate
 		li $a1, 45			# Store y coordinate
 		jal bitmap_address		# Compute start address
+		la $s1, platformState		# Load memory address of platformState
+		sw $s0, 20($s1)			# Store location of third platform into memory
 		add $a0, $s0, $zero		# Store start address
-		li $a1, 46			# Store size of platform
+		li $a1, 46			# Define size of platform
+		sw $a1, 24($s1)			# Store size of platform into memory
 		jal draw_platform		# Draw platform
-		
-		# Draw first enemy
-		li $a0, 0			# Store x coordinate
-		li $a1, 14			# Store y coordinate
-		jal bitmap_address		# Compute start address
-		add $a0, $s0, $zero		# Store start address
-		jal draw_enemy			# Draw enemy
-			
-		# Draw second enemy
-		li $a0, 53			# Store x coordinate
-		li $a1, 27			# Store y coordinate
-		jal bitmap_address		# Compute start address
-		add $a0, $s0, $zero		# Store start address
-		jal draw_enemy			# Draw enemy
-		
-		# Draw third enemy
-		li $a0, 52			# Store x coordinate
-		li $a1, 42			# Store y coordinate
-		jal bitmap_address		# Compute start address
-		add $a0, $s0, $zero		# Store start address
-		jal draw_enemy			# Draw enemy
-		
-		# Draw player
-		li $a0, 21			# Store x coordinate
-		li $a1, 40			# Store y coordinate
-		jal bitmap_address		# Compute start address
-		add $a0, $s0, $zero		# Store start address
-		jal draw_player			# Draw player
 		
 		# Draw bomb
 		li $a0, 45			# Store x coordinate
@@ -123,7 +122,153 @@ main:		# Draw first platform
 		jal bitmap_address		# Compute start address
 		add $a0, $s0, $zero		# Store start address
 		jal draw_bomb			# Draw bomb
+			
+		# Draw first enemy
+		li $a0, 0			# Store x coordinate
+		li $a1, 14			# Store y coordinate
+		jal bitmap_address		# Compute start address
+		la $s1, enemyState		# Load memory address of enemyState
+		li $a1, 3			# Number of enemies for this level
+		sw $a1, 0($s1)			# Store number of enemies at start of array
+		sw $s0, 4($s1)			# Store location of enemy in enemyState
+		add $a0, $s0, $zero		# Define start address
+		jal draw_enemy			# Draw enemy
+			
+		# Draw second enemy
+		li $a0, 53			# Store x coordinate
+		li $a1, 27			# Store y coordinate
+		jal bitmap_address		# Compute start address
+		la $s1, enemyState		# Load memory address of enemyState
+		sw $s0, 12($s1)			# Store location of enemy in enemyState
+		add $a0, $s0, $zero		# Define start address
+		jal draw_enemy			# Draw enemy
 		
+		# Draw third enemy
+		li $a0, 40			# Store x coordinate
+		li $a1, 42			# Store y coordinate
+		jal bitmap_address		# Compute start address
+		la $s1, enemyState		# Load memory address of enemyState
+		sw $s0, 20($s1)			# Store location of enemy in enemyState
+		add $a0, $s0, $zero		# Define start address
+		jal draw_enemy			# Draw enemy
+		
+		# Draw player
+		li $a0, 21			# Store x coordinate
+		li $a1, 40			# Store y coordinate
+		jal bitmap_address		# Compute start address
+		la $s1, playerState		# Load the memory address that stores location of player
+		sw $s0, 0($s1)			# Store location of player in memory
+		add $a0, $s0, $zero		# Store start address
+		jal draw_player			# Draw player
+		
+main:		# Check for collision with enemies
+		la $a1, playerState		# Get address of playerState
+		lw $a0, 0($a1)			# Load current location of player into $a0
+		jal xy_address			# Calculate xy-coordinates for player
+		add $t1, $s0, $zero		# $t1 stores x-coordinate of player
+		add $t2, $s1, $zero		# $t2 stores y-coordinate of player
+		
+		la $s7, enemyState		# $s7 stores address of enemyState
+		lw $t0, 0($s7)			# $t0 stores number of enemies
+		addi $s7, $s7, 4		# $s7 stores address of enemy
+		
+ce_loop:	beqz $t0, keyboard		# When done checking all enemies, ce_fin
+
+		lw $a0, 0($s7)			# $a0 stores location of enemy
+		jal xy_address			# Calculate xy-coordinates for enemy
+		add $t3, $s0, $zero		# $t3 stores x-coordinate of enemy
+		add $t4, $s1, $zero		# $t4 stores y-coordinate of enemy
+		
+		addi $t5, $t2, 4		# $t5 stores bottom y-coordinate of player
+		addi $t6, $t4, 2		# $t6 stores bottom y-coordinate of enemy
+		bne $t5, $t6, ce_skip		# Check collision by y-coordinate
+		addi $t5, $t1, 3
+		add $t6, $t3, $zero
+		beq $t5, $t6, collision		# Check collision by x-coordinate right of player
+		addi $t5, $t1, -3
+		addi $t6, $t4, 2
+		beq $t5, $t6, collision		# Check collision by x-coordinate left of player
+		j ce_skip			# If reaches here, no collision
+collision:	la $a1, playerState		# Get address of playerState
+		lw $a0, 4($a1)			# Load current player health
+		addi $a0, $a0, -1		# Decrease health of player
+		blez $a0, fail			# If out of health, fail
+		sw $a0, 4($a1)			# Store new health into memory
+		li $v0, 1			# Print health to debug
+		syscall
+		li $v0, 4
+		la $a0, newline
+		syscall
+		
+		# Check if player is standing on platform
+		
+		
+		j keyboard		
+ce_skip:	addi $s7, $s7, 8		# $a0 stores address of next enemy
+		addi $t0, $t0, -1		# Decrement $t0 (number of enemies left to check)
+		j ce_loop
+				
+keyboard:	li $t9, 0xffff0000 		# Store address of keystroke event
+		lw $t8, 0($t9)  		# Check for keystroke event
+		beq $t8, 1, pressed		# If some key is pressed, branch to pressed
+		
+		li $v0, 32			# Sleep
+		li $a0, SLEEP_TIME
+		j main				# Repeat main
+		
+pressed:	lw $t8, 4($t9)			# Read what key is pressed
+		beq $t8, RIGHT, go_right
+		beq $t8, LEFT, go_left
+		beq $t8, JUMP, jump
+		j main
+		
+go_right:	la $a1, playerState		# Get address of player location
+		lw $a0, 0($a1)			# Load current location of player into $a0
+		lw $a2, 0($a1)			# Load current location of player into $a2
+		jal xy_address			# Compute current xy-coordinates of player
+		addi $s0, $s0, 3		# Compute next rightmost x coordinate of player
+		bge $s0, SIZE_BY_UNIT, main	# If next x coordinate goes beyond screen, do nothing
+		add $a0, $a2, $zero		# Restore current address
+		jal erase_player		# Erase player at current location
+		addi $a0, $a0, 4		# Get new address after key pressed
+		sw $a0, 0($a1)			# Store new address in memeory
+		jal draw_player			# Draw player at new location
+		j main
+		
+go_left:	la $a1, playerState		# Get address of player location
+		lw $a0, 0($a1)			# Load current location of player into $a0
+		lw $a2, 0($a1)			# Load current location of player into $a2
+		jal xy_address			# Compute current xy-coordinates of player
+		subi $s0, $s0, 3		# Compute next leftmost x coordinate of player
+		blt $s0, 0, main		# If next x coordinate goes beyond screen, do nothing
+		add $a0, $a2, $zero		# Restore current address
+		jal erase_player		# Erase player at current location
+		subi $a0, $a0, 4		# Get new address after key pressed
+		sw $a0, 0($a1)			# Store new address in memeory
+		jal draw_player			# Draw player at new location
+		j main
+		
+jump:		la $a1, playerState		# Get address of player location
+		lw $a0, 0($a1)			# Load current location of player into $a0
+		jal xy_address			# Compute current xy-coordinates of player
+		subi $s2, $s1, JUMP_HEIGHT
+		bltz $s2, jump_max
+jump_rep:	blt $s1, $s2, main
+jump_once:	la $a1, playerState		# Get address of player location
+		lw $a0, 0($a1)			# Load current location of player into $a0
+		jal erase_player		# Erase player at current location
+		subi $a0, $a0, SIZE_BY_BYTE	# Compute new location (one row above)
+		jal draw_player			# Draw player at new location
+		sw $a0, 0($a1)			# Store new location
+		jal xy_address			# Calculate xy-coordinates
+		li $v0, 32			# Sleep for a bit to see animation
+		li $a0, SLEEP_TIME
+		syscall
+		j jump_rep			# Repeat jumping once until reach height
+jump_max:	addi $s2, $zero, 1		# If player is near top, use a ceil for jump height
+		j jump_rep
+		
+fail:		jal erase_screen
 		# End program
 		li $v0, 10
 		syscall
@@ -138,6 +283,14 @@ bitmap_address:	li $s0, SIZE_BY_UNIT
 		add $s0, $s0, $a0
 		sll $s0, $s0, 2
 		addi $s0, $s0, BASE_ADDRESS
+		jr $ra
+		
+xy_address:	subi $a0, $a0, BASE_ADDRESS	# Get offset from (0, 0)
+		sra $a0, $a0, 2			# Divide by 4 to get (y*width + x)
+		li $s0, SIZE_BY_UNIT		
+		div $a0, $s0			# Divide by width to get x and y
+		mfhi $s0			# Remainder is x
+		mflo $s1			# Quotient is y
 		jr $ra
 
 # Arguments:	Start address	$a0
@@ -210,3 +363,43 @@ draw_bomb:	li $s0, BOMB			# Load color for bomb
 		sw $s0, 512($a0)
 		jr $ra
 		
+# Arguments:	Start address	$a0
+# Registers:	Color value	$s0
+# Returns:	Erase player at $a0	
+erase_player:	li $s0, ERASE			# Load line color to erase
+		sw $s0, 0($a0)
+		sw $s0, 4($a0)
+		sw $s0, 252($a0)
+		sw $s0, 256($a0)
+		sw $s0, 260($a0)
+		sw $s0, 508($a0)
+		sw $s0, 516($a0)
+		sw $s0, 520($a0)
+		sw $s0, 760($a0)
+		sw $s0, 764($a0)
+		sw $s0, 776($a0)
+		sw $s0, 1016($a0)
+		sw $s0, 1032($a0)
+		sw $s0, 512($a0)
+		sw $s0, 768($a0)
+		sw $s0, 772($a0)
+		sw $s0, 1020($a0)
+		sw $s0, 1024($a0)
+		sw $s0, 1028($a0) 
+		jr $ra
+
+# Arguments:	None
+# Registers:	Color value	$s0
+# 		Current cell	$s1
+#		Last cell	$s2
+# Returns:	All screen reset
+erase_screen:	li $s0, ERASE
+		li $s1, BASE_ADDRESS
+		addi $s2, $s1, 16380
+es_loop:	bgt $s1, $s2, es_fin
+		sw $s0, 0($s1)
+		addi $s1, $s1, 4
+		j es_loop
+es_fin:		jr $ra
+		
+
